9b917f7c0ea6b2f8983b519fe2ea868d39bfdde3
HoanPhamSJSU <69431005+HoanPhamSJSU@users.noreply.github.com>
2021-01-16 21:46:05 -0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,10 +28,4 @@ const port = process.env.PORT || 7000
 app.set('port', process.env.PORT || 3000);
 app.set('host', "localhost" || '34.94.79.201');
 
-app.listen(port, () => {
-  console.log(`Server is running on ${port}`)
-})
-
-http.createServer(app).listen(app.get('port'), app.get('host'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+app.listen(port, () => console.log(`App listening on port ${port}!`))
